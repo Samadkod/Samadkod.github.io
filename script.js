cd8f2c496622b25af6948778ede7a79df475926b
Samadou KODON <121679170+Samadkod@users.noreply.github.com>
2024-11-04 01:13:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Script pour la fonctionnalité de bascule du menu
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const aside = document.querySelector('aside');
+const menuToggle = document.querySelector('.menu-toggle');
+const aside = document.querySelector('aside');
 
-    // Écouteur d'événements pour le bouton de bascule du menu
-    menuToggle.addEventListener('click', () => {
-        aside.classList.toggle('active');
-    });
+menuToggle.addEventListener('click', () => {
+    aside.classList.toggle('active');
+});
+
